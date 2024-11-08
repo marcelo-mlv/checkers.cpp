cce1495d8332e56016208d547e175528f803357b
Marcelo <marceloloiolaveras@gmail.com>
2024-11-08 04:10:39 -0300

    refactor: minor changes

--- classes.hpp
+++ classes.hpp
@@ -9,19 +9,23 @@ enum Piece {
 };
 
 class Board {
-    public:
+    private:
+        friend std::ostream& operator<<(std::ostream& os, const Board& board);
         Piece grid[8][8];
         int n_wins[2];
-        void print_grid();
-        friend std::ostream& operator<<(std::ostream& os, const Board& board);
-    Board();
+    public:
+        Board();
 };
 
 Board::Board() {
+    n_wins[white] = 0;
+    n_wins[black] = 0;
+    
     for(int i = 0; i < 8; i++)
         for(int j = 0; j < 8; j++)
             grid[i][j] = NONE;
     
+    // Generating black/white pieces
     for(int i = 0; i < 3; i++) {
         for(int j = 0; j < 4; j++) {
             int exp = 2*j+(i+1)%2;
@@ -32,7 +36,6 @@ Board::Board() {
 }
 
 std::ostream& operator<<(std::ostream& os, const Board& board) {
-    std::cout << std::endl;
     for(int i = 0; i < 8; i++) {
         for(int j = 0; j < 8; j++)
             os << static_cast<char>(visuals[board.grid[i][j]]) << " "; 
